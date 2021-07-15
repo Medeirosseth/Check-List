bff4080ef18598f49d43c24fee15fa6c536e3336
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-15 11:28:20 -0700

    add completed functionality

--- check-list/src/App.js
+++ check-list/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-
 import Form from './components/Form'
 import CheckList from './components/CheckList';
 
@@ -18,7 +17,10 @@ function App() {
       todos={todos}
       setTodos={setTodos}
        />
-      <CheckList />
+      <CheckList 
+      todos={todos} 
+      setTodos={setTodos}
+      />
     </div>
   );
 }
--- check-list/src/components/CheckList.js
+++ check-list/src/components/CheckList.js
@@ -1,10 +1,21 @@
 import React from "react"
+import Todo from "./Todo"
 
-
-const CheckList = () => {
+const CheckList = ({ todos, setTodos }) => {
+  
   return (
     <div className="checklist-container">
-      <ul className="checklist-list"></ul>
+      <ul className="checklist-list">
+        {todos.map((todo) => (
+          <Todo 
+          todos={todos}
+          setTodos={setTodos}
+          key={todo.id} 
+          text={todo.text} 
+          todo={todo}
+          />
+        ))}
+      </ul>
     </div>
   )
 
